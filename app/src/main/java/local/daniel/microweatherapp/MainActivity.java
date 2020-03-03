46933e1da762dc06114e195ead7ec1663baa21bc
Daniel Arroyo <darroyo@avla.com>
2020-03-03 18:43:17 -0300

    ADD: city search try

--- app/src/main/java/local/daniel/microweatherapp/MainActivity.java
+++ app/src/main/java/local/daniel/microweatherapp/MainActivity.java
@@ -3,9 +3,12 @@ package local.daniel.microweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import local.daniel.microweatherapp.model.WeatherData;
-import local.daniel.microweatherapp.model.WeatherDetail;
 import local.daniel.microweatherapp.retrofit.OpenWeatherMap;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -18,7 +21,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private static OkHttpClient httpClient = new OkHttpClient();
-
+    TextView tempTxt;
+    Button searchBtn;
+    EditText cityBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,16 +37,35 @@ public class MainActivity extends AppCompatActivity {
                 .client(httpClient)
                 .build();
 
-        OpenWeatherMap openWeatherMap = retrofit.create(OpenWeatherMap.class);
-        Call<WeatherData> weatherCall = openWeatherMap.getWeather("London");
+        cityBox = (EditText) findViewById(R.id.cityBox);
+        searchBtn = (Button) findViewById(R.id.search);
+        tempTxt = (TextView) findViewById(R.id.temp);
 
-        System.out.println(weatherCall.request().url().toString());
+        OpenWeatherMap openWeatherMap = retrofit.create(OpenWeatherMap.class);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("clicked");
+                refreshCityWeather(openWeatherMap, cityBox.getText().toString());
+            }
+        });
+     
+
+
+
+
+    }
+
+    private void refreshCityWeather(OpenWeatherMap openWeatherMap, String cityName) {
+        Call<WeatherData> weatherCall = openWeatherMap.getWeather(cityName);
 
         weatherCall.enqueue(new Callback<WeatherData>() {
             @Override
             public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
                 System.out.println("success");
-                System.out.println(response.body().getMain().getTemp());
+                WeatherData weatherData = response.body();
+                tempTxt.setText(weatherData.getMain().getTemp().toString());
             }
 
             @Override
@@ -50,8 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("failed =(");
             }
         });
-
-
     }
 
     private HttpUrl buildWeatherUrl() {
